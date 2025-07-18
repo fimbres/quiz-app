a9368957ad320e64638c74d7e75ad37ef0a3547a
Isaac <fimbresisaac23@gmail.com>
2025-07-18 01:06:05 -0700

    implementing context

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
 import './index.css'
 import App from './App.tsx'
+import { QuizContextProvider } from './store/context.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QuizContextProvider>
+      <App />
+    </QuizContextProvider>
   </StrictMode>,
 )
